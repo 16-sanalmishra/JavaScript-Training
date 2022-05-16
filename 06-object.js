6837c7af33ebf1c2235f7d7dc46d4f248e428831
Sanal Mishra <16-sanalmishra>
2022-05-16 11:11:26 +0530

    JavaScrit Objects

--- 06-object.js
+++ 06-object.js
@@ -13,3 +13,16 @@ function greet(person){
 
 }
 greet(p)
+console.log(greet.name);
+
+function greet() {
+    console.log('hi');
+  }
+  
+  greet();
+  
+  console.log(greet);
+  
+  greet.language = 'espanol';
+  console.log(greet);
+  console.log(greet.language);
